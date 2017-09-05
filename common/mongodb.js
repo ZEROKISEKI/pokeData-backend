650cabb40958d9bc5d55c84aaa05d20032a9b262
ZEROKISEKI <20142100095@m.scnu.edu.cn>
2017-09-05 16:35:16 +0800

    [format](common): format ora text

--- common/mongodb.js
+++ common/mongodb.js
@@ -9,7 +9,7 @@ const db = mongoose.createConnection(`mongodb://${mongodb.host}:${mongodb.port}/
 
 autoIncrement.initialize(db)
 
-db.on('open', () => spinner.succeed(`数据库连接成功`))
-db.on('error', err => spinner.fail(`数据库连接出错，错误原因: ${err.message}`))
+db.on('open', () => spinner.succeed(`数据库连接成功\n`))
+db.on('error', err => spinner.fail(`数据库连接出错，错误原因: ${err.message}\n`))
 
 export default db
