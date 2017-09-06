6637dd46aadad33cf5d585182f0b4325ed6d4e3e
ZEROKISEKI <20142100095@m.scnu.edu.cn>
2017-09-06 12:40:45 +0800

    [refactor](common): replace mongoose mypromise to bluebird

--- common/mongodb.js
+++ common/mongodb.js
@@ -2,14 +2,24 @@ import mongoose from 'mongoose'
 import autoIncrement from 'mongoose-auto-increment'
 import { mongodb } from "../config"
 import ora from 'ora'
+import blueBird from 'bluebird'
 
-const spinner = ora('启动数据库...').start()
+// 替换My Promise为BlueBird
+mongoose.Promise = blueBird
 
-const db = mongoose.createConnection(`mongodb://${mongodb.host}:${mongodb.port}/${mongodb.database}`)
+const spinner = ora('启动数据库...\n').start()
+
+const db = mongoose.connect(`mongodb://${mongodb.host}:${mongodb.port}/${mongodb.database}`)
 
 autoIncrement.initialize(db)
 
-db.on('open', () => spinner.succeed(`数据库连接成功\n`))
-db.on('error', err => spinner.fail(`数据库连接出错，错误原因: ${err.message}\n`))
+db.then(() => {
+  spinner.succeed(`数据库连接成功\n`)
+}).catch(err => {
+  spinner.fail(`数据库连接出错，错误原因: ${err.message}\n`)
+})
+
+// db.on('open', () => spinner.succeed(`数据库连接成功\n`))
+// db.on('error', err => spinner.fail(`数据库连接出错，错误原因: ${err.message}\n`))
 
 export default db
