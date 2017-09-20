0cdeff612cfc6d8d3c9df7e00e853d22ab1f6017
ZEROKISEKI <20142100095@m.scnu.edu.cn>
2017-09-20 21:09:25 +0800

    [test](index): add test entry

--- tests/index.js
+++ tests/index.js
@@ -1,23 +1,24 @@
 import Test from '../utils/test'
 import ora from 'ora'
 
-before('数据库连接', async () => {
-  const spinner = ora('进行数据库连接').start()
-  try {
-	await Test.connectDB()
-    spinner.succeed('数据库连接成功！')
-  } catch (err) {
-    spinner.fail(`数据库连接失败! 错误原因: ${err.message}`)
-  }
-  // run()
-})
+describe('DataBase', () => {
+  before('数据库连接', async () => {
+	const spinner = ora('进行数据库连接').start()
+	try {
+	  await Test.connectDB()
+	  spinner.succeed('数据库连接成功！')
+	} catch (err) {
+	  spinner.fail(`数据库连接失败! 错误原因: ${err.message}`)
+	}
+  })
 
-after('关闭数据库连接', async () => {
-  const spinner = ora('关闭数据库连接').start()
-  try {
-	await Test.closeDB()
-	spinner.succeed('成功关闭数据库连接！')
-  } catch (err) {
-	spinner.fail(`关闭连接失败! 错误原因: ${err.message}`)
-  }
+  after('关闭数据库连接', async () => {
+	const spinner = ora('关闭数据库连接').start()
+	try {
+	  await Test.closeDB()
+	  spinner.succeed('成功关闭数据库连接！')
+	} catch (err) {
+	  spinner.fail(`关闭连接失败! 错误原因: ${err.message}`)
+	}
+  })
 })
