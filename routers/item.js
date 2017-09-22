32acce38ee56a1d5d340d65b9b7a7baa90a0efd9
ZEROKISEKI <20142100095@m.scnu.edu.cn>
2017-09-22 12:24:04 +0800

    [feat](router): complete item module routers

--- routers/item.js
+++ routers/item.js
@@ -1,10 +1,22 @@
 import Router from 'koa-router'
 import Item from '../controllers/item'
+import jwt from '../middlewares/jwt'
 
 const router = new Router({
   prefix: '/item'
 })
 
-router.get('/:id', Item.getItemById)
+router.use(jwt({
+  path: []
+}))
+
+router.get('/all', Item.getItems)
+router.get('/search', Item.searchItems)
+
+router.post('/add', Item.addItem)
+router.post('/remove', Item.removeItem)
+
+router.get('/:id([0-9]+)', Item.getItemById)
+router.post('/:id([0-9]+)', Item.updateItem)
 
 export default router
