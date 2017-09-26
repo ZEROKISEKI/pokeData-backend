902c9f6db91b1bef54e033787662dc81c36e1bec
ZEROKISEKI <20142100095@m.scnu.edu.cn>
2017-09-26 14:15:22 +0800

    [feat](page): add page module routers and template(for webpack)

--- routers/page.js
+++ routers/page.js
@@ -1,1 +1,15 @@
 // page.js为backend客户端界面入口点
+
+import Router from 'koa-router'
+
+const router = new Router({
+  prefix: '/page'
+})
+
+// 采用ejs模板引擎, 读取assets.json的静态资源(utils.assetWebpack)
+router.get('/all', async ctx => {
+  ctx.type = 'text/html'
+  ctx.body = await ctx.render('page.html')
+})
+
+export default router
