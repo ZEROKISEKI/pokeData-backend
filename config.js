ea7f616b334b7d10f1f7009697cef4ec326b0e2f
ZEROKISEKI <20142100095@m.scnu.edu.cn>
2017-09-26 14:10:00 +0800

    [style](code): fix config.js code style

--- config.js
+++ config.js
@@ -1,37 +1,37 @@
 export const mongodb = {
-	host: 'localhost',
-	port: 27017,
-	database: 'pokeData-test'
+  host: 'localhost',
+  port: 27017,
+  database: 'pokeData-test'
 }
 
 // 按照命令行的顺序来
 export const proto = {
-	'files': [
-		'./protos/BasicMessage.proto',
-		'./protos/Common.proto',
-		'./protos/User.proto',
-		'./protos/Item.proto',
-		'./protos/Person.proto',
-		'./protos/Skill.proto',
-		'./protos/Pokemon.proto'
-	],
-	'-t': 'static-module',
-	'-w': 'commonjs',
-	'-o': './common/model.js',
-	* [Symbol.iterator]() {
-		let result = []
-		for (const key of Object.keys(this)) {
-			if (this[key] === null) {
-				result.push(key)
-			} else if (key !== 'files') {
-				result.splice(result.length, 0, key, this[key])
-			}
-		}
-		result = [...result, ...this['files']]
-		yield result
-	}
+  'files': [
+    './protos/BasicMessage.proto',
+    './protos/Common.proto',
+    './protos/User.proto',
+    './protos/Item.proto',
+    './protos/Person.proto',
+    './protos/Skill.proto',
+    './protos/Pokemon.proto'
+  ],
+  '-t': 'static-module',
+  '-w': 'commonjs',
+  '-o': './common/model.js',
+  * [Symbol.iterator]() {
+    let result = []
+    for (const key of Object.keys(this)) {
+      if (this[key] === null) {
+        result.push(key)
+      } else if (key !== 'files') {
+        result.splice(result.length, 0, key, this[key])
+      }
+    }
+    result = [...result, ...this['files']]
+    yield result
+  }
 }
 
 export const jwt = {
-	secret: 'DL3F7OXg0Wy2gha+5O+XEQ=='
+  secret: 'DL3F7OXg0Wy2gha+5O+XEQ=='
 }
