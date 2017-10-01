5faa8c37f455ed4540ed24253389e8924d973d89
ZEROKISEKI <20142100095@m.scnu.edu.cn>
2017-10-01 13:06:09 +0800

    [style](eslint): add switchCase indent 2 space

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
 		'html'
 	],
   "rules": {
-    "indent": ["error", 2],
+    "indent": ["error", 2, { "SwitchCase": 1 }],
     "linebreak-style": ["error", "unix"],
     "quotes": ["error", "single"]
   }
